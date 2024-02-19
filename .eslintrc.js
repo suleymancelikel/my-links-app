b92c3642c4fb670d9822c10578e7a84aee6c5adc
suleymancelikel <suleymancelikell@gmail.com>
2024-02-19 21:55:29 +0300

    Edited Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['airbnb', 'airbnb/hooks'],
+  parser: 'babel-eslint',
+  // parserOptions: {
+  //   ecmaVersion: 2017,
+  //   sourceType: 'module',
+  //   ecmaFeatures: {
+  //     experimentalObjectRestSpread: true,
+  //     jsx: true,
+  //   },
+  // },
+  // rules: {
+  //   strict: 'warn',
+  //   'import/extensions': [
+  //     'error',
+  //     'ignorePackages',
+  //     {
+  //       js: 'never',
+  //       jsx: 'never',
+  //       ts: 'never',
+  //       tsx: 'never',
+  //     },
+  //   ],
+  //   'no-shadow': [
+  //     false,
+  //     'ignorePackages',
+  //     {
+  //       allow: 'false',
+  //     },
+  //   ],
+  // },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
+  "semi": true,
   trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
 };
